0bd126878140b62e4a193a97993fed241ae623bc
Abhi15k <96977894+Abhi15k@users.noreply.github.com>
2024-09-28 23:37:11 +0530

    updated the files

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -1,22 +1,22 @@
 package com.mockcompany.webapp.service;
 
-import com.mockcompany.webapp.data.ProductItemRepository;
+// import com.mockcompany.webapp.data.ProductItemRepository;
 import com.mockcompany.webapp.model.ProductItem;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.Collections; // Import statement added
+import java.util.Collections; 
 
 @Service
 public class SearchService {
 
-    private final ProductItemRepository productItemRepository;
+    // private final ProductItemRepository productItemRepository;
 
-    @Autowired
-    public SearchService(ProductItemRepository productItemRepository) {
-        this.productItemRepository = productItemRepository;
-    }
+    // @Autowired
+    // public SearchService(ProductItemRepository productItemRepository) {
+    //     this.productItemRepository = productItemRepository;
+    // }
 
     public Collection<ProductItem> search(String query) {
         // Break the tests by always returning an empty list
